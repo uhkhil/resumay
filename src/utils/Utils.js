c9826e199414adb2e8b7fde9f142c18e6799ef18
Akhil <akhilnareshkumar@gmail.com>
2019-12-03 18:36:38 +0530

    Make cleaner files for Education.

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -7,26 +7,6 @@ export const JSONifyFormData = (formData) => {
     return obj;
 }
 
-export const addHistory = (form) => {
-    const oldForm = JSON.parse(JSON.stringify(form));
-    let newForm;
-    const addHistoryToChild = (child, history, idx) => {
-        switch (child.type) {
-            case 'array':
-                child.children = child.children.map((nextChild, nextChildId) => addHistoryToChild(nextChild, history + 'a' + nextChildId + '-', null))
-                return child
-            case 'group':
-                child.fields = child.fields.map((nextChild, nextChildId) => addHistoryToChild(nextChild, history, nextChildId))
-                return child
-            default:
-                child.history = history + idx;
-                return child
-        }
-    }
-    newForm = addHistoryToChild(oldForm, '', 0)
-    return newForm;
-}
-
 export const cloner = obj => JSON.parse(JSON.stringify(obj));
 
 export const humanifyDate = (dateString) => {
